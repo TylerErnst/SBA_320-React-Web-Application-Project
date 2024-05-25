66780059f2a19252ad4195a3e0c56a27ce3834c5
TylerErnst <tcernst5@gmail.com>
2024-05-24 23:33:49 -0600

    more testing

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,30 @@ import { useState, useEffect } from "react";
 import "./App.css";
 
 // Import our components
-import PokemonDisplay from "./components/PokemonDisplay";
-import Form from "./components/Form";
+// import PokemonDisplay from "./components/PokemonDisplay";
+// import Form from "./components/Form";
 
 
 export default function App() {
 
-  // State to hold pokemon data
-  const [pokemon, setPokemon] = useState(null);
+  // // State to hold pokemon data
+  // const [pokemon, setPokemon] = useState(null);
 
-  // Function to get pokemon
-  const getPokemon = async(searchTerm) => {
-    try {
-      // Make fetch request and store the response
-      const response = await fetch(
-        `http://pokeapi.co/api/v2/pokemon/${searchTerm}`
-      );
-      // Parse JSON response into a JavaScript object
-      const data = await response.json();
-      // Set the Pokemon state to the received data
-      setPokemon(data);
-    } catch(e) {
-      console.error(e)
-    }
-  };
+  // // Function to get pokemon
+  // const getPokemon = async(searchTerm) => {
+  //   try {
+  //     // Make fetch request and store the response
+  //     const response = await fetch(
+  //       `http://pokeapi.co/api/v2/pokemon/${searchTerm}`
+  //     );
+  //     // Parse JSON response into a JavaScript object
+  //     const data = await response.json();
+  //     // Set the Pokemon state to the received data
+  //     setPokemon(data);
+  //   } catch(e) {
+  //     console.error(e)
+  //   }
+  // };
 
    // This will run on the first render but not on subsquent renders
    useEffect(() => {
@@ -36,7 +36,7 @@ export default function App() {
   // We pass pokemon as props to pokemon display
   return (
     <div className="App">
-      hi
+      Pokemon Search
       {/* <Form pokemonSearch={getPokemon} /> */}
       {/* <PokemonDisplay pokemon={pokemon} /> */}
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-
-// Wrap the App component with the BrowserRouter component to enable the router features.
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
