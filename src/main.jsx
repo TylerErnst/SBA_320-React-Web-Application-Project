b60cf2f2cab1b3666989aa2577f18c7ee50bd17d
TylerErnst <tcernst5@gmail.com>
2024-05-24 23:19:01 -0600

    cleaned up main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-// Import BrowserRouter
-import { BrowserRouter } from 'react-router-dom'
 
 // Wrap the App component with the BrowserRouter component to enable the router features.
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
 )
